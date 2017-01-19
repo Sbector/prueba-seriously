ce9ae42db4b9ac3dc51839e4e122c7e823c5515c
Sbector <saulbector@gmail.com>
2017-01-18 22:57:58 -0600

    Añadir efecto blur

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
 var video;
 
 function setup() {
-  canvas = createCanvas (640, 480);
+  canvas = createCanvas (640, 480, WEBGL);
+  canvas.id('p5canvas');
   background(51);
   video = createCapture(VIDEO);
   video.size(640, 480);
+  video.id('p5video')
 
   var seriously = new Seriously();
+
+  var src = seriously.source('#p5video');
+  var target = seriously.target('#p5canvas');
+
+  var blur = seriously.effect('blur');
+  //blur.amount = '#blur-range';
+  blur.source = src;
+  target.source = blur;
+
+
 
   seriosly.go();
 
