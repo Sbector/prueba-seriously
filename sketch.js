ac9af76ebfbc600f4fff0af5a4ce768c98a915a5
Sbector <saulbector@gmail.com>
2017-01-18 23:31:33 -0600

    Revert "Copiar código de Daniel"
    This reverts commit edf24a0d45677b0fe7ca7173cdb5986be68235d3.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var video;
-var slider;
 
 function setup() {
   canvas = createCanvas(640, 480, WEBGL);
@@ -7,32 +6,19 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.id('p5video');
-  //video.hide();
-  //slider = createSlider(0, 1, 0.5, 0.01);
-  //slider.id('blur-slider');
 
   var seriously = new Seriously();
 
   var src = seriously.source('#p5video');
   var target = seriously.target('#p5canvas');
 
-  // var blur = seriously.effect('blur');
-  // blur.amount = '#blur-slider';
-  // blur.source = src;
-  // target.source = blur;
-
-  var chroma = seriously.effect('chroma');
-  chroma.source = src;
-  target.source = chroma;
-  var r = 98 / 255;
-  var g = 175 / 255;
-  var b = 116 / 255;
-  chroma.screen = [r,g,b,1];
+  var blur = seriously.effect('blur');
+  //blur.amount = '#blur-range';
+  blur.source = src;
+  target.source = blur;
 
 
 
-
-
-  seriously.go();
+  seriosly.go();
 
 }
