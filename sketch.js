c2890bf626ec2f8b07daadf515f4d3fc498292cf
Sbector <saulbector@gmail.com>
2017-01-18 22:45:00 -0600

    Inicializar seriously en sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+var video;
+
 function setup() {
-  
+  canvas = createCanvas (640, 480);
+  background(51);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+
+  var seriously = new Seriously();
+
+  seriosly.go();
+
 }
 
 function draw() {
-  
+
 }
