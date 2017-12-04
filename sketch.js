c3a9f860751e9079d64f5e264693bcf11c592176
No_otra_vez <saulbector@gmail.com>
2017-12-03 23:39:08 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,16 @@
-var video;
+var capture;
 
 function setup() {
-  canvas = createCanvas(640, 480, WEBGL);
-  canvas.id('p5canvas');
-  video = createCapture({
+  createCanvas(640, 480);
+  var constraints = {
     audio: false,
     video: {
-      facingMode: "environment"
+      facingMode: "user"
     }
-  });
-  video.size(640, 480);
-  video.id('p5video');
-
-  var seriously = new Seriously();
-
-  var src = seriously.source('#p5video');
-  var target = seriously.target('#p5canvas');
-
-  //var blur = seriously.effect('blur');
-  //blur.amount = '#blur-range';
-  //blur.source = src;
-  //target.source = blur;
-
-  var vignette = seriously.effect('vignette');
-  vignette.source = src;
-  target.source = vignetteM
-
-
-  seriosly.go();
-
+  };
+  capture = createCapture(constraints);
+}
+function draw() {
+  image(capture, 0, 0, 640, 480);
+  // filter(INVERT); // ;-)
 }
