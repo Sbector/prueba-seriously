44abffc92f24f515e762af9387ff6895c564f885
No_otra_vez <saulbector@gmail.com>
2017-12-12 16:56:40 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,8 @@ var capture;
 
 function setup() {
   createCanvas(640, 480);
-  var constraints = {
-    audio: false,
-    video: {
-      facingMode: "user"
-    }
-  };
-  capture = createCapture(constraints);
+  
+  capture = createCapture(VIDEO,640,480);
 }
 function draw() {
   image(capture, 0, 0, 640, 480);
