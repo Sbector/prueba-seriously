7189cf4afda7464574fe15b1c898fc6ac209862b
No_otra_vez <saulbector@gmail.com>
2017-12-12 17:00:30 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
-var capture;
+var video;
 
 function setup() {
-  createCanvas(640, 480);
-  
-  capture = createCapture(VIDEO,640,480);
-}
-function draw() {
-  image(capture, 0, 0, 640, 480);
-  // filter(INVERT); // ;-)
+  canvas = createCanvas(640, 480, WEBGL);
+  canvas.id('p5canvas');
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.id('p5video');
+
+  var seriously = new Seriously();
+
+  var src = seriously.source('#p5video');
+  var target = seriously.target('#p5canvas');
+
+  //var blur = seriously.effect('blur');
+  //blur.amount = '#blur-range';
+  //blur.source = src;
+  //target.source = blur;
+
+  var vignette = seriously.effect('vignette');
+  vignette.source = src;
+  target.source = vignetteM
+
+
+  seriosly.go();
+
 }
